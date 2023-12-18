b879ef3cd43aa05d2c2ff1973277af5e4d7c39a9
Toni97 <ayovcheva75@gmail.com>
2023-12-18 15:02:13 +0000

    changes to code structure and logic

--- starter/script.js
+++ starter/script.js
@@ -88,87 +88,96 @@ const upperCasedCharacters = [
   'Z'
 ];
 
-const charOptions = [];
-const generatedPassword = '';
-// You can store the generatedPassword as a string and concat each character OR
-// as an array and push each character, then join once you have enough characters
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+
+  // You can store the generatedPassword as a string and concat each character OR
+  // as an array and push each character, then join once you have enough characters
   // Prompt for password length//
-    // Conditional to check that the number that was entered is in range//
+  // Conditional to check that the number that was entered is in range//
   // Prompts store data as strings, so need to parse into a number//
   // If the user's input is out of range, call the function again//
-  let getLength;
-  getLength = parseInt(
+  
+ let getLength = parseInt(
     prompt("Choose a password length between 8 and 128 char.")
   );
-  if (getLength > 7 && getLength < 129) {
-    charOptions.length = getLength;
-  } else {
+
+  if (getLength < 8 || getLength > 128) {
+  
     alert("Invalid input. Choose a number between 8 and 128.");
-    getPasswordOptions;
+    // getPasswordOptions;
     return;
-  }
+  } 
 
   //Prompt to choose special char and cap letter as boolean
-    // Confirm which character sets to use//
-  function getSpecial() {//Create nested function to avoid a user going back to selecting password length//
-    let getCapital = confirm("Include upper case letters?");
-    let getLowerCase = confirm("Include lower case letters?");
-    let getNum = confirm("Include numbers?");
-    let getSpecialChar = confirm("Include special characters?");
+  // Confirm which character sets to use//
+  //Create nested function to avoid a user going back to selecting password length//
+  let getCapital = confirm("Include upper case letters?");
+  let getLowerCase = confirm("Include lower case letters?");
+  let getNum = confirm("Include numbers?");
+  let getSpecialChar = confirm("Include special characters?");
 
-    // Once they select a character set:
-    // If the user answers false for all, either return out of the function or call the function again//
+  // Once they select a character set:
+  // If the user answers false for all, either return out of the function or call the function again//
   // Generate a random character for each selected character set//
   // Either push selected character sets to a mega-array of all selected characters//
   // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-    if (getCapital === false && getLowerCase === false && getNum === false && getSpecialChar === false) {
-      alert("Please select at least one character type.");
-      getSpecial(); // Call the function again to prompt the user for character types
-      return;
-    } else if (getCapital === true) {
-      const indexCapital = Math.floor(Math.random() * upperCasedCharacters.length);
-      charOptions.push(indexCapital);
-      console.log(charOptions);
-    } else if (getLowerCase === true) {
-      const indexLowerCase = Math.floor(Math.random() * lowerCasedCharacters.length);
-      charOptions.push(indexLowerCase);
-      console.log(charOptions);
-    } else if (getNum === true) {
-      const indexNum = Math.floor(Math.random() * numericCharacters.length);
-      charOptions.push(indexNum);
-      console.log(charOptions);
-    } else if (getSpecialChar === true) {
-      const indexSpecialChar = Math.floor(Math.random() * specialCharacters.length);
-      charOptions.push(indexSpecialChar);
-      console.log(charOptions);
-    }
-  }
-  getSpecial()
+  if (getCapital === false && getLowerCase === false && getNum === false && getSpecialChar === false) {
+    alert("Please select at least one character type.");
+  // Call the function again to prompt the user for character types
+    return;
+  } 
 
+  return {
+    getLength: getLength,
+    getCapital: getCapital,
+    getLowerCase: getLowerCase,
+    getNum:  getNum,
+    getSpecialChar: getSpecialChar
+  }
   
+ 
 }
-getPasswordOptions()
 
 // Function for getting a random element from an array
 function getRandom(arr) {
   // Need a variable to hold the password as it's being generated//
   // Need a variable to hold the index that's being generated
-
+  const indexChar = arr[Math.floor(Math.random() * arr.length)];
   // For loop that loops the number of times that matches the length the user chose
   // Generate a random number
   // That number is the index for a character in the mega-array
   // So then, mega-array[generated-index] is the actual character
   // Add that character to the password
-
+return indexChar
   // Once we finish the for loop, return the generated password
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  let charOptions = [];
+  let generatedPassword = '';
+  const options = getPasswordOptions()
 
+  if (options.getCapital === true) {
+   charOptions = charOptions.concat(upperCasedCharacters)
+   console.log(charOptions)
+  } else if (options.getLowerCase === true) {
+    
+  } else if (options.getNum === true) {
+   
+  } else if (options.getSpecialChar === true) {
+    
+  }
+
+  for (let i = 0; i < options.getLength; i++) {
+   const randomLetter = getRandom(charOptions)
+  console.log(randomLetter);
+  }
+
+
+  console.log(generatedPassword); 
 }
 
 // Get references to the #generate element
